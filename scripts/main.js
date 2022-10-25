002c122dc3922672b43e3caaf732c4272bfa6ffd
mrpatches123 <mr.patches@ymail.com>
2022-10-25 12:51:37 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,21 @@
 import { world } from "@minecraft/server";
-// console.warn('hello');
-// //world.ejhjhe();
-// import { http, HttpHeader, HttpRequest, HttpRequestMethod } from "mojang-net";
+console.warn('hello');
+//world.ejhjhe();
+import { http, HttpHeader, HttpRequest, HttpRequestMethod } from "mojang-net";
 
-// const port = 8080;
-// let request = new HttpRequest(`http://localhost:${port}`);
-// request.method = HttpRequestMethod.POST;
-// req.headers = [
-// 	new HttpHeader('Content-Type', 'text/plain')
-// ];
-// request.body = 'hi';
-// request.timeout = 100;
+const port = 8080;
+let request = new HttpRequest(`http://localhost:${port}`);
+request.method = HttpRequestMethod.POST;
+req.headers = [
+	new HttpHeader('Content-Type', 'text/plain')
+];
+request.body = 'hi';
+request.timeout = 100;
 
-// async function test() {
-// 	let res = await http.request(request);
-// 	console.warn(JSON.stringify(res));
-// };
+async function test() {
+	let res = await http.request(request);
+	console.warn(JSON.stringify(res));
+};
 
 world.events.tick.subscribe(({ currentTick }) => {
 
